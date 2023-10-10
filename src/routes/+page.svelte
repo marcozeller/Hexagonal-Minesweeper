58457c212a702a6130b5664e6200de49fb7fbbaa
mzeller@ethz.ch <marcozeller@bluewin.ch>
2023-10-11 01:05:09 +0200

    first tests for hexagon field

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let size_field = 5;
+    let field = Array(size_field).fill(Array(size_field).fill({}));
+</script>
+
+<div class="field">
+{#each field as row, i}
+	<div class="row">
+    {#each row as cell, j}
+		<div class="hexagon pointy"> {i}, {j}</div>
+    {/each}
+	</div>
+{/each}
+</div>
+
+<style>
+    .field {
+        display: flex;
+        flex-direction: column;}
+    .row {
+        display: flex;
+        flex-direction: row;
+        margin-bottom: -28px;
+    }
+    .row:nth-child(odd){
+        margin-left: 51px;
+    }
+    .row:nth-child(even){
+    }
+	.hexagon {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+		color: #fff;
+
+		background: #111;
+		margin: 1px;
+		transition: 2s;
+	}
+	.hexagon.flat {
+		clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
+		height: 100px;
+		width: 110px;
+	}
+	.hexagon.pointy {
+		clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+		height: 110px;
+		width: 100px;
+	}
+</style>
